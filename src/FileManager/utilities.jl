c889e652034a4bda378eb06caef77e3fdb21c5ab
marteresagh <marteresa28@gmail.com>
2020-10-17 15:03:46 +0200

    fix utilities

--- src/FileManager/utilities.jl
+++ src/FileManager/utilities.jl
@@ -1,7 +1,7 @@
 """
 Read file line by line.
 """
-function getdirectories(filepath::String)
+function get_directories(filepath::String)
 	return readlines(filepath)
 end
 
@@ -10,21 +10,19 @@ end
 
 Read volume model from a file JSON.
 """
-function boxmodelfromjson(volume::String)
+function boxmodel_from_json(volume::String)
 	V,CV,FV,EV = PointClouds.volumemodelfromjson(volume)
 	return V,EV,FV
 end
 
 """
-	boxmodelfromaabb(aabb::Tuple{Array{Float64,2},Array{Float64,2}})
-
 Return LAR model of the aligned axis box defined by `aabb`.
 """
-function boxmodelfromaabb(aabb::Tuple{Array{Float64,2},Array{Float64,2}})
+function boxmodel_from_aabb(aabb::AABB)
 	min,max = aabb
-	V = [	min[1]  min[1]  min[1]  min[1]  max[1]  max[1]  max[1]  max[1];
-		 	min[2]  min[2]  max[2]  max[2]  min[2]  min[2]  max[2]  max[2];
-		 	min[3]  max[3]  min[3]  max[3]  min[3]  max[3]  min[3]  max[3] ]
+	V = [	aabb.x_min  aabb.x_min  aabb.x_min  aabb.x_min  aabb.x_max  aabb.x_max  aabb.x_max  aabb.x_max;
+		 	aabb.y_min  aabb.y_min  aabb.y_max  aabb.y_max  aabb.y_min  aabb.y_min  aabb.y_max  aabb.y_max;
+		 	aabb.z_min  aabb.z_max  aabb.z_min  aabb.z_max  aabb.z_min  aabb.z_max  aabb.z_min  aabb.z_max ]
 	EV = [[1, 2],  [3, 4], [5, 6],  [7, 8],  [1, 3],  [2, 4],  [5, 7],  [6, 8],  [1, 5],  [2, 6],  [3, 7],  [4, 8]]
 	FV = [[1, 2, 3, 4],  [5, 6, 7, 8],  [1, 2, 5, 6],  [3, 4, 7, 8],  [1, 3, 5, 7],  [2, 4, 6, 8]]
 	return V,EV,FV
@@ -36,18 +34,20 @@ end
 Return LAR model (V,EV,FV) of a box, aligned or not to axes.
 """
 function getmodel(bbin::String)
-	return PointClouds.boxmodelfromjson(bbin)
+	return boxmodel_from_json(bbin)
 end
 
-function getmodel(bbin::Tuple{Array{Float64,2},Array{Float64,2}})
-	return PointClouds.boxmodelfromaabb(bbin)
+function getmodel(bbin::Array{Float64,1})
+	# in questo formato gli viene passato -> bbin = [x_min y_min z_min x_max y_max z_max ]
+	bb = AABB(bbin[4], bbin[1], bbin[5], bbin[2], bbin[6], bbin[3])
+	return boxmodel_from_aabb(bb)
 end
 
 
 """
 file tfw
 """
-function savetfw(output::String, GSD::Float64, lx::Float64, uy::Float64)
+function save_tfw(output::String, GSD::Float64, lx::Float64, uy::Float64)
 	fname = splitext(output)[1]
 	io = open(fname*".tfw","w")
 	write(io, "$(Float64(GSD))\n")
