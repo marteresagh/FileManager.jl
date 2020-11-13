6dd88451fae7777251bbe47d7c852adfd9c520a1
marteresagh <marteresa28@gmail.com>
2020-11-13 15:49:39 +0100

    generalize fun

--- src/Save/txt.jl
+++ src/Save/txt.jl
@@ -1,15 +1,21 @@
+"""
+Save points by row in file .txt.
+"""
 function save_points_txt(filename::String,V::Lar.Points)
 	io = open(filename,"w")
 
 	for i in 1:size(V,2)
-		write(io, "$(V[1,i]) $(V[2,i]) $(V[3,i])\n")
+		for j in 1:size(V,1)
+			write(io, "$(V[j,i]) ")
+		end
+		write(io, "\n")
 	end
 
 	close(io)
 end
 
 """
-save points of segment by row
+Save extrema points of segment by row.
 """
 function save_lines_txt(filename::String, lines::Array{Hyperplane,1}, affine_matrix::Matrix)
 	io = open(filename,"w")
@@ -24,27 +30,25 @@ function save_lines_txt(filename::String, lines::Array{Hyperplane,1}, affine_mat
 end
 
 """
+Save point cloud by row: "x y z r g b".
 """
-#TODO da scrivire generica
-function savePlane(hyperplane::Hyperplane, filename::String)
-	# plane2json(plane::Plane, filename::String)  JSON FORMAT
-	io = open(filename,"w")
-	write(io, "$(plane.normal[1]) $(plane.normal[2]) $(plane.normal[3]) ")
-	write(io, "$(plane.centroid[1]) $(plane.centroid[2]) $(plane.centroid[3])")
-	close(io)
-end
-
-
 function save_points_rgbs_txt(filename::String, PC::PointCloud)
 	io = open(filename,"w")
 	RGB = convert(Array{Int32,2},floor.(PC.rgbs*255))
 	for i in 1:PC.n_points
 		V = PC.coordinates
-		write(io, "$(V[1,i]) $(V[2,i]) $(V[3,i]) $(RGB[1,i]) $(RGB[2,i]) $(RGB[3,i])\n")
+		for j in 1:PC.dimension
+			write(io, "$(V[j,i]) ")
+		end
+		for j in 1:3
+			write(io, "$(RGB[j,i]) ")
+		end
+		write(io, "\n")
 	end
 
 	close(io)
 end
+
 
 """
 file tfw
@@ -60,5 +64,16 @@ function save_tfw(output::String, GSD::Float64, lx::Float64, uy::Float64)
 	U=@sprintf("%f", uy)
 	write(io, "$L\n")
 	write(io, "$U\n")
+	close(io)
+end
+
+"""
+"""
+#TODO da scrivire generica
+function savePlane(hyperplane::Hyperplane, filename::String)
+	# plane2json(plane::Plane, filename::String)  JSON FORMAT
+	io = open(filename,"w")
+	write(io, "$(plane.normal[1]) $(plane.normal[2]) $(plane.normal[3]) ")
+	write(io, "$(plane.centroid[1]) $(plane.centroid[2]) $(plane.centroid[3])")
 	close(io)
 end
