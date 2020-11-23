228ab8414d2e530bf7a82f510a56801c35a4494f
marteresagh <marteresa28@gmail.com>
2020-11-23 10:13:50 +0100

    docstring

--- src/struct.jl
+++ src/struct.jl
@@ -1,4 +1,15 @@
 """
+Potree project metadata:
+ - version:  the version number is needed to know how to interpret the data,
+ - octreeDir: the name of the directory where files of octree are contained,
+ - tightBoundingBox: it contains the minimum and maximum coordinates of the bounding box,
+ - boundingBox: it contains the minimum and maximum coordinates of the cubic bounding box aligned to the axes which contains the root node,
+ - pointAttributes: declares the format of points. It can be LAS, LAZ or in the case of binary format it contains an array of attributes such as `["POSITION_CARTESIAN", "COLOR_PACKED", "INTENSITY"]`,
+ - spacing: spacing value at the root node,
+ - hierarchyStepSize: the number of levels in each chunk.
+ 
+# Example
+
 {
     "version": "1.7",
     "octreeDir": "data",
@@ -25,8 +36,21 @@
     "scale": 0.001,
     "hierarchyStepSize": 5
 }
-"""
 
+# Fields
+```jldoctest
+version           :: String
+octreeDir         :: String
+projection        :: String
+points            :: Int64
+boundingBox       :: Common.AABB
+tightBoundingBox  :: Common.AABB
+pointAttributes   :: String
+spacing           :: Float64
+scale             :: Float64
+hierarchyStepSize :: Int32
+```
+"""
 struct CloudMetadata
     version::String
     octreeDir::String
