87ebefdade4363c4ad07ce755d557fe51e2ccb97
marteresagh <marteresa28@gmail.com>
2020-12-16 15:37:38 +0100

    update mkdir

--- src/utilities.jl
+++ src/utilities.jl
@@ -51,13 +51,19 @@ function mkdir_project(folder::String, project_name::String)
 	@assert isdir(folder) "$folder not an existing folder"
 	proj_folder = joinpath(folder,project_name)
 
-	if !isdir(proj_folder)
-		mkdir(proj_folder)
-	end
+	mkdir_if(proj_folder)
 
 	return proj_folder
 end
 
+"""
+folder
+"""
+function mkdir_if(folder::String)
+	if !isdir(folder)
+		mkdir(folder)
+	end
+end
 
 """
 	clearfolder(folder::String)
@@ -73,4 +79,33 @@ function clearfolder(folder::String)
 		rm(joinpath(root,file))
 	end
 	return 1
+end
+
+
+"""
+generate input point cloud
+"""
+function source2pc(source::String, lod::Union{Nothing,Int64})
+
+	if isdir(source) # se source è un potree
+		Detection.flushprintln("Potree struct")
+		cloud_metadata = Detection.CloudMetadata(source)
+
+		if lod == -1
+			trie = potree2trie(source)
+			max_level = FileManager.max_depth(trie)
+			all_files = FileManager.get_all_values(trie)
+			PC = FileManager.las2pointcloud(all_files...)
+			return PC
+		else
+			all_files = FileManager.get_files_in_potree_folder(source,lod)
+			PC = FileManager.las2pointcloud(all_files...)
+			return PC
+		end
+
+	elseif isfile(source) # se source è un file
+		PC = FileManager.las2pointcloud(source)
+		return PC
+	end
+
 end
