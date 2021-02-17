26e4b675b9a9412501acf1a03549a765846a99c2
marteresagh <marteresa28@gmail.com>
2021-02-17 15:14:51 +0100

    batch update

--- examples/debug.jl
+++ examples/debug.jl
@@ -1,8 +1,3 @@
 using Common
+using Visualization
 using FileManager
-
-filename = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/sezioneUIPotree.las"
-filename = "C:/Users/marte/Documents/potreeDirectory/pointclouds/SEZIONE_POTREE"
-
-PC = FileManager.source2pc(filename,-1)
-cp(filename,"slice.las"; force::Bool=false, follow_symlinks::Bool=false)
