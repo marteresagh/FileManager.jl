1e6d10f9b99ead8c4ef6e9ea0107ae2122bf9b64
marteresagh <marteresa28@gmail.com>
2021-03-01 10:57:28 +0100

    update function for reading data

--- examples/debug.jl
+++ examples/debug.jl
@@ -10,66 +10,22 @@ GL.VIEW([
 ])
 
 
-NAME_PROJ = "MURI"
+NAME_PROJ = "MURI_LOD3"
 folder = "C:/Users/marte/Documents/GEOWEB/TEST"
 
-function read_data_vect2D(folder::String,NAME_PROJ::String)
-	OBBs = Volume[]
-	hyperplanes = Hyperplane[]
-	alpha_shapes = Lar.LAR[]
-	for (root, dirs, files) in walkdir(joinpath(folder,NAME_PROJ))
-		for dir in dirs
-			if dir!="PLANES"
-				folder_plane = joinpath(root,dir)
+dirs, hyperplanes, OBBs, alpha_shapes, full_inliers = FileManager.read_data_vect2D(folder::String,NAME_PROJ::String)
 
-				io = open(joinpath(folder_plane,"finite_plane.txt"), "r")
-				lines = readlines(io)
-				close(io)
-
-				b = [tryparse.(Float64,split(lines[i], " ")) for i in 1:length(lines)]
-				normal = [b[1][1],b[1][2],b[1][3]]
-				centroid = normal*b[1][4]
-				inliers = FileManager.load_points(joinpath(folder_plane,"inliers.txt"))
-
-				hyperplane = Hyperplane(PointCloud(inliers[1:3,:],inliers[4:6,:]), normal, centroid)
-				OBB = Volume([b[2][1],b[2][2],b[2][3]],[b[3][1],b[3][2],b[3][3]],[b[4][1],b[4][2],b[4][3]])
-
-				V = FileManager.load_points(joinpath(folder_plane,"boundary_points.txt"))
-				EV = FileManager.load_connected_components(joinpath(folder_plane,"boundary_edges.txt")) #FileManager.load_cells(joinpath(folder_plane,"boundary_edges.txt"))
-				model = (V,EV)
-				push!(hyperplanes,hyperplane)
-				push!(OBBs,OBB)
-				push!(alpha_shapes,model)
-			end
-		end
+function draw_all(hyperplanes, OBBs)
+	mesh = []
+	centroid = Common.centroid(hyperplanes[1].inliers.coordinates)
+	for i in 1:length(hyperplanes)
+		V,EV,FV = getmodel(OBBs[i])
+		points = hyperplanes[i].inliers.coordinates
+		col = GL.COLORS[rand(1:12)]
+		push!(mesh,GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,col));
+		push!(mesh,	GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),points)'),col));
 	end
-
-	las_full_inliers = FileManager.searchfile(joinpath(joinpath(folder,NAME_PROJ),"PLANES"),".las")
-
-	###############################################################
-	# for file in las_full_inliers
-	# 	h,_ = LasIO.FileIO.load(file)
-	# 	if h.records_count > 1000
-	# 		PC = FileManager.las2pointcloud(file)
-	# 		points = PC.coordinates
-	# 		aabb = Common.boundingbox(points)
-	# 		obb = Common.ch_oriented_boundingbox(points)
-	# 		plane = Plane(points)
-	# 		push!(planes,plane)
-	# 		push!(AABBs,aabb)
-	# 		push!(OBBs,obb)
-	# 	end
-	# end
-	###############################################################
-
-	return hyperplanes, OBBs, alpha_shapes, las_full_inliers
+	return mesh
 end
 
-hyperplanes, OBBs, alpha_shapes = FileManager.read_data_vect2D(folder::String,NAME_PROJ::String)
-
-V,FV = Common.DrawPlanes(hyperplanes; box_oriented = false)
-centroid = Common.centroid(V)
-
-GL.VIEW([
-	GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,GL.COLORS[1],0.8)
-])
+GL.VIEW(draw_all(hyperplanes, OBBs))
--- src/utilities.jl
+++ src/utilities.jl
@@ -114,59 +114,51 @@ end
 """
 	Read data in vectorized_2D folder results.
 """
-
 function read_data_vect2D(folder::String,NAME_PROJ::String)
 	OBBs = Volume[]
 	hyperplanes = Hyperplane[]
 	alpha_shapes = Lar.LAR[]
+ 	folders = String[]
 	las_full_inliers = nothing
+
 	for (root, dirs, files) in walkdir(joinpath(folder,NAME_PROJ))
 		for dir in dirs
-			if dir!="PLANES"
-				folder_plane = joinpath(root,dir)
-
-				io = open(joinpath(folder_plane,"finite_plane.txt"), "r")
-				lines = readlines(io)
-				close(io)
-
-				b = [tryparse.(Float64,split(lines[i], " ")) for i in 1:length(lines)]
-				normal = [b[1][1],b[1][2],b[1][3]]
-				centroid = normal*b[1][4]
-				inliers = FileManager.load_points(joinpath(folder_plane,"inliers.txt"))
-
-				hyperplane = Hyperplane(PointCloud(inliers[1:3,:],inliers[4:6,:]), normal, centroid)
-				OBB = Volume([b[2][1],b[2][2],b[2][3]],[b[3][1],b[3][2],b[3][3]],[b[4][1],b[4][2],b[4][3]])
-
-				V = FileManager.load_points(joinpath(folder_plane,"boundary_points.txt"))
-				EV = FileManager.load_connected_components(joinpath(folder_plane,"boundary_edges.txt")) #FileManager.load_cells(joinpath(folder_plane,"boundary_edges.txt"))
-				model = (V,EV)
-				push!(hyperplanes,hyperplane)
-				push!(OBBs,OBB)
-				push!(alpha_shapes,model)
-			end
+			folder_plane = joinpath(root,dir)
+			push!(folders, folder_plane)
 		end
 	end
 
-	full_inliers_folder = joinpath(joinpath(folder,NAME_PROJ),"PLANES")
-	if isdir(full_inliers_folder)
-		las_full_inliers = FileManager.searchfile(full_inliers_folder,".las")
+	n_planes = length(folders)
+	for i in 1:n_planes
+		println("$i folder processed of $n_planes" )
+		io = open(joinpath(folders[i],"finite_plane.txt"), "r")
+		lines = readlines(io)
+		close(io)
+
+		b = [tryparse.(Float64,split(lines[i], " ")) for i in 1:length(lines)]
+		normal = [b[1][1],b[1][2],b[1][3]]
+		centroid = normal*b[1][4]
+		inliers = FileManager.load_points(joinpath(folders[i],"inliers.txt"))
+
+		hyperplane = Hyperplane(PointCloud(inliers[1:3,:],inliers[4:6,:]), normal, centroid)
+		OBB = Volume([b[2][1],b[2][2],b[2][3]],[b[3][1],b[3][2],b[3][3]],[b[4][1],b[4][2],b[4][3]])
+
+		V = FileManager.load_points(joinpath(folders[i],"boundary_points.txt"))
+		EV = FileManager.load_connected_components(joinpath(folders[i],"boundary_edges.txt")) #FileManager.load_cells(joinpath(folder_plane,"boundary_edges.txt"))
+		model = (V,EV)
+		push!(hyperplanes,hyperplane)
+		push!(OBBs,OBB)
+		push!(alpha_shapes,model)
+
+		full_inliers = 	joinpath(folders[i],"full_inliers.las")
+		if isfile(full_inliers)
+			push!(las_full_inliers,full_inliers)
+		end
+
+		# if full alpha shapes
+		# get model 
 	end
 
-	###############################################################
-	# for file in las_full_inliers
-	# 	h,_ = LasIO.FileIO.load(file)
-	# 	if h.records_count > 1000
-	# 		PC = FileManager.las2pointcloud(file)
-	# 		points = PC.coordinates
-	# 		aabb = Common.boundingbox(points)
-	# 		obb = Common.ch_oriented_boundingbox(points)
-	# 		plane = Plane(points)
-	# 		push!(planes,plane)
-	# 		push!(AABBs,aabb)
-	# 		push!(OBBs,obb)
-	# 	end
-	# end
-	###############################################################
 
-	return hyperplanes, OBBs, alpha_shapes, las_full_inliers
+	return folders, hyperplanes, OBBs, alpha_shapes, las_full_inliers
 end
