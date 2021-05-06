6ec0a98625c3ddfbd1135d3ce81216394d8ed89f
marteresagh <marteresa28@gmail.com>
2021-05-06 14:59:31 +0200

    update test

--- test/las.jl
+++ test/las.jl
@@ -1,6 +1,7 @@
 @testset "LAS" begin
     workdir = dirname(@__FILE__)
     file = joinpath(workdir,"Files/PC.las")
+    @show file
     PC = FileManager.PointCloud(rand(3,100),LasIO.FixedPointNumbers.N0f16.(rand(3,100)))
     FileManager.save_pointcloud(file, PC,  "TEST")
     PC_load = FileManager.source2pc(file)
