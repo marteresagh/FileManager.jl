e5a6ecc997eb89980a21a7c02543c5c6a05a70a0
marteresagh <marteresa28@gmail.com>
2021-06-04 15:45:56 +0200

    add test

--- examples/las.jl
+++ examples/las.jl
@@ -35,10 +35,9 @@ L = readlas(file) #17.592 ms
 @btime V = las2larpoints(file) #68.525 ms
 
 
-
 @btime V = FileManager.las2color(file) #1.406 s
-
 @btime V = las2color(file) #31.809 ms
 
 
-@btime V = FileManager.las2pointcloud(file) #119.670 ms
+@btime V = FileManager.las2pointcloud(file) #2.681 s
+@btime V = las2pointcloud(file) #119.670 ms
