b7abf21b8ca4669903ea8425e7bd2ffed77a0e60
marteresagh <marteresa28@gmail.com>
2021-06-04 15:42:25 +0200

    test

--- examples/las.jl
+++ examples/las.jl
@@ -3,3 +3,6 @@ using FileManager
 file = raw"C:\Users\marte\Documents\GEOWEB\TEST\REGISTRATION\TEST_casaletto.las"
 
 aabb = FileManager.las2aabb(file)
+
+using BenchmarkTools
+@btime V = FileManager.las2pointcloud(file) #2.681 ms
