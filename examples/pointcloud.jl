c42c30d2a7c6f8ac276a4755a6146de0515e84f9
marteresagh <marteresa28@gmail.com>
2021-08-05 10:59:39 +0200

    fix example

--- examples/pointcloud.jl
+++ examples/pointcloud.jl
@@ -16,16 +16,14 @@ end
 
 potree = "D:/potreeDirectory/pointclouds/CASALETTO"
 cloudMets = FileManager.CloudMetadata(potree)
+
 vector_t = [cloudMets.boundingBox.x_min,cloudMets.boundingBox.y_min,cloudMets.boundingBox.z_min]
+
 trie = FileManager.potree2trie(potree)
 max_level = FileManager.max_depth(trie)
 files = FileManager.get_files_in_potree_folder(potree,0, false)
 leaves = FileManager.get_leaf(trie)
 # PC = FileManager.las2pointcloud(files...)
-
-
-files = [raw"C:\Users\marte\Documents\Julia_package\package_test\TEST\FILEMANAGER\potree2las.las"]
-
 affineMatrix = Common.t(-vector_t...)
 
 mesh = view_octree(files,affineMatrix)
