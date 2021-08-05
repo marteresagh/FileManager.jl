3ae063271175f1e6eab34d92ce42c26b8ac21123
marteresagh <marteresa28@gmail.com>
2021-08-05 10:59:13 +0200

    add more sources + compute full aabb and total points

--- examples/pointcloud.jl
+++ examples/pointcloud.jl
@@ -1,15 +1,32 @@
 using FileManager
 using Visualization
+using Common
 
-potree = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CUPOLA"
+function view_octree(files, affineMatrix)
+    mesh = []
+    for file in files
+        octree = FileManager.las2aabb(file)
+        V,EV,FV = Common.getmodel(octree)
+        PC = FileManager.las2pointcloud(file)
+        push!(mesh,Visualization.points(Common.apply_matrix(affineMatrix,PC.coordinates), PC.rgbs))
+        push!(mesh,Visualization.GLGrid(Common.apply_matrix(affineMatrix,V),EV,Visualization.COLORS[7]))
+    end
+    return mesh
+end
+
+potree = "D:/potreeDirectory/pointclouds/CASALETTO"
 cloudMets = FileManager.CloudMetadata(potree)
+vector_t = [cloudMets.boundingBox.x_min,cloudMets.boundingBox.y_min,cloudMets.boundingBox.z_min]
 trie = FileManager.potree2trie(potree)
 max_level = FileManager.max_depth(trie)
-files = FileManager.get_files_in_potree_folder(potree,2, true)
-PC = FileManager.las2pointcloud(files...)
+files = FileManager.get_files_in_potree_folder(potree,0, false)
+leaves = FileManager.get_leaf(trie)
+# PC = FileManager.las2pointcloud(files...)
 
-Visualization.VIEW(
-    [
-    Visualization.points(PC.coordinates, PC.rgbs)
-    ]
-)
+
+files = [raw"C:\Users\marte\Documents\Julia_package\package_test\TEST\FILEMANAGER\potree2las.las"]
+
+affineMatrix = Common.t(-vector_t...)
+
+mesh = view_octree(files,affineMatrix)
+Visualization.VIEW(mesh)
--- script/potree2las.jl
+++ script/potree2las.jl
@@ -7,68 +7,121 @@ using FileManager
 println("packages OK")
 
 
+"""
+	get_potree_dirs(txtpotreedirs::String)
+
+Return collection of potree directories.
+"""
+function get_potree_dirs(txtpotreedirs::String)
+    @show txtpotreedirs
+    if isfile(txtpotreedirs)
+        @show "sono qui"
+        return FileManager.readlines(txtpotreedirs)
+    elseif isdir(txtpotreedirs)
+        return [txtpotreedirs]
+    end
+end
+
+
 function parse_commandline()
-	s = ArgParseSettings()
+    s = ArgParseSettings()
 
-	@add_arg_table! s begin
-	"potree"
-		help = "Potree"
-		arg_type = String
-		required = true
-	"--output", "-o"
-		help = "output file .las"
-		arg_type = String
-		required = true
-	end
+    @add_arg_table! s begin
+        "potrees"
+            help = "Potree projects directory"
+            arg_type = String
+            required = true
+        "--output", "-o"
+            help = "output file .las"
+            arg_type = String
+            required = true
+    end
 
-	return parse_args(s)
+    return parse_args(s)
 end
 
 
 function main()
-	args = parse_commandline()
+    args = parse_commandline()
 
-	potree = args["potree"]
-	output = args["output"]
+    txtpotreedirs = args["potrees"]
+    output = args["output"]
 
-	flushprintln("")
-	flushprintln("== PARAMETERS ==")
-	flushprintln("Potree  =>  $potree")
-	flushprintln("Output  =>  $output")
-	flushprintln("")
-	flushprintln("== PROCESSING ==")
-	# save new LAS source
+    sources = get_potree_dirs(txtpotreedirs)
+    flushprintln("")
+    flushprintln("== PARAMETERS ==")
 
-	cloudmetadata = FileManager.CloudMetadata(potree)
-	aabb = cloudmetadata.tightBoundingBox
-	n_points = cloudmetadata.points
-	trie = FileManager.potree2trie(potree)
-	node_files = FileManager.get_all_values(trie)
+    for i in eachindex(sources)
+        println("source[$i]: $(sources[i])")
+    end
 
-	# creo l'header
-	pointsProcessed = 0
-	mainHeader = FileManager.newHeader(aabb,"Potree2Las",FileManager.SIZE_DATARECORD,n_points)
-	# apro il las
-	t = open(output,"w")
-		write(t, FileManager.LasIO.magic(FileManager.LasIO.format"LAS"))
-		write(t,mainHeader)
-		for file in node_files
-			s = open(file, "r")
-				h, laspoints = FileManager.read_LAS_LAZ(file) # read file
-				for laspoint in laspoints # read each point
-					plas = FileManager.newPointRecord(laspoint,h,FileManager.LasIO.LasPoint2,mainHeader)
-					write(t,plas) # write this record on temporary file
-					flush(t)
-					pointsProcessed += 1
-					if pointsProcessed % 1_000_000 == 0
-						println("$pointsProcessed points processed of $n_points")
-					end
-				end
-			close(s)
-		end
-	close(t)
+    flushprintln("Output  =>  $output")
 
-	FileManager.successful(true,splitdir(output)[1])
+    # creo l'header: mi serve il bb che contiene tutto il modello e il numero di punti totali
+    full_aabb = AABB()
+    n_points_total = 0
+    for potree in sources
+        cloudmetadata = FileManager.CloudMetadata(potree)
+        aabb = cloudmetadata.tightBoundingBox
+        Common.update_boundingbox!(full_aabb, aabb)
+        n_points_total += cloudmetadata.points
+    end
+
+    flushprintln("")
+    flushprintln("AABB:")
+    flushprintln("min: [$(full_aabb.x_min),$(full_aabb.y_min),$(full_aabb.z_min)]")
+    flushprintln("max: [$(full_aabb.x_max),$(full_aabb.y_max),$(full_aabb.z_max)]")
+
+    flushprintln("")
+    flushprintln("Points: $n_points_total")
+
+    mainHeader = FileManager.newHeader(
+        full_aabb,
+        "Potree2Las",
+        FileManager.SIZE_DATARECORD,
+        n_points_total
+    )
+
+    t = open(output, "w")
+    write(t, FileManager.LasIO.magic(FileManager.LasIO.format"LAS"))
+    write(t, mainHeader)
+
+    flushprintln("")
+    flushprintln("== PROCESSING ==")
+    # per ogni potree
+    for potree in sources
+        pointsProcessed = 0
+        cloudmetadata = FileManager.CloudMetadata(potree)
+        n_points = cloudmetadata.points
+        trie = FileManager.potree2trie(potree)
+        node_files = FileManager.get_all_values(trie)
+
+
+        # apro il las
+        for file in node_files
+            s = open(file, "r")
+            h, laspoints = FileManager.read_LAS_LAZ(file) # read file
+            for laspoint in laspoints # read each point
+                plas = FileManager.newPointRecord(
+                    laspoint,
+                    h,
+                    FileManager.LasIO.LasPoint2,
+                    mainHeader,
+                )
+                write(t, plas) # write this record on temporary file
+                flush(t)
+                pointsProcessed += 1
+                if pointsProcessed % 1_000_000 == 0
+                    println("$pointsProcessed points processed of $n_points")
+                end
+            end
+            close(s)
+        end
+
+    end
+
+    close(t)
+    FileManager.successful(true, splitdir(output)[1])
 end
 
 @time main()
